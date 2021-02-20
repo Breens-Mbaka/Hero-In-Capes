c7e074027136448817390bc64c3f2e32bca83495
Breens-Thomas <mbakabreens@gmail.com>
2021-02-20 11:23:06 +0300

    Added bootstrap for styling

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,15 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) {
-        //testing to see if user can make a hero
-        Hero hero1 = new Hero("Spider man", 23);
-        System.out.println(hero1.age);
-
-        Squad heroes = new Squad("Avengers","Fighting for justice");
-        System.out.println(heroes.squadName);
-        System.out.println(heroes.cause);
+       get("/", (request,response)->{
+           Map<String, Object> model = new HashMap<>();
+           return new ModelAndView(model, "index.hbs");
+       }, new HandlebarsTemplateEngine());
     }
 }
