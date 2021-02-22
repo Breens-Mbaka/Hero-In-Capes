0c2adf3bd89ca69f2a522884873fe2a9c75620a6
Breens-Thomas <mbakabreens@gmail.com>
2021-02-22 11:09:53 +0300

    Retrieve data from our forms

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,11 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
-import static spark.Spark.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 public class App {
     public static void main(String[] args) {
@@ -14,5 +15,19 @@ public class App {
            Map<String, Object> model = new HashMap<>();
            return new ModelAndView(model, "index.hbs");
        }, new HandlebarsTemplateEngine());
+
+       get("/heroes", (request,response)-> {
+           Map<String, Object> model = new HashMap<>();
+           String name = request.queryParams("name");
+           String age = request.queryParams("age");
+           String weakness = request.queryParams("weakness");
+           String strength = request.queryParams("strength");
+
+           model.put("name",name);
+           model.put("age",age);
+           model.put("weakness",weakness);
+           model.put("strength",strength);
+           return new ModelAndView(model,"squad.hbs");
+       },new HandlebarsTemplateEngine());
     }
 }
