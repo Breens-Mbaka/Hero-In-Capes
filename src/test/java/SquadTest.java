27decaa87808bbd8d5faa7014f8188cced456b04
Breens-Thomas <mbakabreens@gmail.com>
2021-02-23 09:23:54 +0300

    Add test for instatiating Squad class

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -3,5 +3,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SquadTest {
-
+    @Test
+    void testingHeroObjectIsInstanceOfHeroClass_true() {
+        Squad testSquad = new Squad("name","laughing");
+        assertEquals(true, testSquad instanceof Squad);
+    }
 }
