41fb5d97cd51fcd4037bb9814718361d380b261c
Breens-Thomas <mbakabreens@gmail.com>
2021-02-23 09:20:26 +0300

    Add a Squad Class

---
+++ src/main/java/Squad.java
@@ -0,0 +1,17 @@
+public class Squad {
+    public String squadName;
+    public String cause;
+
+    public Squad(String squadName, String cause) {
+        this.squadName = squadName;
+        this.cause = cause;
+    }
+
+    public String getSquadName() {
+        return squadName;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+}
